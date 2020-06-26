bc3e29d1275c935c7b36e325ef40abf89d54f676
Brian Jubiakto Murthyady <murthyady@gmail.com>
2020-06-26 12:09:11 +0700

    Slicing halaman Get Started

---
+++ src/components/atoms/Button/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Button = ({type, title}) => {
+  return (
+    <View style={styles.container(type)}>
+      <Text style={styles.text(type)}>{title}</Text>
+    </View>
+  );
+};
+
+export default Button;
+
+const styles = StyleSheet.create({
+  container: type => ({
+    backgroundColor: type === 'secondary' ? '#FFFFFF' : '#0BCAD4',
+    borderRadius: 10,
+    paddingVertical: 10,
+  }),
+  text: type => ({
+    fontSize: 18,
+    fontWeight: '600',
+    color: type === 'secondary' ? '#112340' : '#FFFFFF',
+    textAlign: 'center',
+  }),
+});
---
+++ src/pages/GetStarted/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {StyleSheet, Text, View, ImageBackground} from 'react-native';
+import {ILLogo, ILGetStarted} from '../../assets';
+import {Button} from '../../components';
+
+const GetStarted = () => {
+  return (
+    <ImageBackground source={ILGetStarted} style={styles.page}>
+      <View>
+        <ILLogo />
+        <Text style={styles.title}>
+          Konsultasi dengan dokter jadi lebih mudah & fleksibel
+        </Text>
+      </View>
+      <View>
+        <Button title="Get Started" />
+        <View style={{height: 16}} />
+        <Button title="Sign In" type="secondary" />
+      </View>
+    </ImageBackground>
+  );
+};
+
+export default GetStarted;
+
+const styles = StyleSheet.create({
+  page: {
+    padding: 40,
+    backgroundColor: 'yellow',
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: '600',
+    color: 'white',
+    marginTop: 91,
+  },
+});
